541363619cdf720c61e66941c11b5fda6fe39444
孙霖霖 <sunlinlin084263@163.com>
2018-07-29 11:18:50 +0800

    20180729 1 新增  init a repo for review js ； use sass ，mintUI in this demo

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+// import css
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+// import 组件
+import App from './App'
+Vue.use(MintUI);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
