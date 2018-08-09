37a595b6cf78059cb94a446228c49818af130f7a
孙霖霖 <sunlinlin084263@163.com>
2018-08-09 15:21:14 +0800

    20180809 1 新增 html脚本化编程 2 canvas部分的内容

--- src/components/chapter_15.vue
+++ src/components/chapter_15.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <div class="page__hd" @click="click_back">this is chapter 15</div>
+    <div class="page__hd text-center" @click="click_back">this is chapter 15</div>
     <div class="page__bd text-center">
-        <canvas ref="canvas" width="300" height="300" >
-
-        </canvas>
+      <!--<img src="../assets/logo.png" alt="">-->
+      <canvas ref="canvas" width="300" height="300">
+      </canvas>
     </div>
   </div>
 </template>
@@ -12,27 +12,81 @@
 <script>
   export default {
     name: 'HelloWorld',
-    data () {
+    data() {
       return {
         msg: 'Welcome to Your Vue.js App'
       }
     },
-    methods:{
-      click_back:function () {
-        this.$router.push({path:'/'})
+    methods: {
+      click_back: function () {
+        this.$router.push({path: '/'})
       },
-      draw:function () {
+      //绘制矩形
+      draw: function () {
         let ctx = this.$refs.canvas.getContext('2d');
         ctx.fillStyle = '#ff0000';
-        ctx.fillRect(10,10,50,50);
+        ctx.fillRect(10, 10, 50, 50);
         ctx.strokeStyle = 'rgba(255,0,0,0.6)';
-        ctx.strokeRect(70,70,50,50);
-        ctx.clearRect(30,30,50,50);
+        ctx.strokeRect(70, 70, 50, 50);
+        ctx.clearRect(30, 30, 50, 50);
+        console.log('ctx', ctx);
+      },
+      // 绘制图片
+      draw_image: function () {
+        let img,
+          canvas,
+          dataURl;
+        canvas = this.$refs.canvas;
+        img = new Image();
+        img.src = require('../assets/logo.png');
+        console.log('img', img);
+        let ctx = this.$refs.canvas.getContext('2d');
+        ctx.drawImage(img, 10, 10, 50, 50);
+        dataURl = canvas.toDataURL();
+        console.log('dataURl',dataURl);
+      },
+      //阴影
+      draw_shadow:function () {
+        let ctx;
+        ctx = this.$refs.canvas.getContext('2d');
+        ctx.shadowOffsetX = 5;
+        ctx.shadowOffsetY = 5;
+        ctx.shadowBlur = 4;
+        ctx.shadowColor = 'rgba(0,0,0,0.5)';
+        ctx.fillStyle = '#ff0000';
+        ctx.fillRect(10,10,50,50);
+        ctx.fillStyle = 'rgba(0,0,255,1)';
+        ctx.fillRect(30,30,50,50);
+      },
+      draw_gradient:function () {
+        let ctx,
+            gradient;
+        ctx = this.$refs.canvas.getContext('2d');
+        gradient = ctx.createLinearGradient(30,30,70,70);
+        gradient.addColorStop(0,"white");
+        gradient.addColorStop(1,"black");
+        ctx.fillStyle = gradient;
+        ctx.fillRect(30,30,50,50);
+
+      },
+      draw_patten:function () {
+        let ctx,img,pattern;
+        ctx = this.$refs.canvas.getContext('2d');
+        img = new Image();
+        img.src = require('../assets/logo.png');
         console.log('ctx',ctx);
+        pattern = ctx.createPattern(img,'repeat');
+        ctx.fillStyle = pattern;
+        ctx.fillRect(10,10,300,300);
+        console.log(ctx.getImageData(10,10,50,50));
       }
     },
     mounted() {
-      this.draw();
+//      this.draw();
+//      this.draw_image();
+//      this.draw_shadow();
+//      this.draw_gradient();
+      this.draw_patten();
     }
   }
 </script>
@@ -42,14 +96,17 @@
   h1, h2 {
     font-weight: normal;
   }
+
   ul {
     list-style-type: none;
     padding: 0;
   }
+
   li {
     display: inline-block;
     margin: 0 10px;
   }
+
   a {
     color: #42b983;
   }
--- src/components/chapter_16.vue
+++ src/components/chapter_16.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="container">
-    <div class="page__hd">this is chapter1</div>
+    <div class="page__hd text-center" @click="click_back">this is chapter 16</div>
     <div class="page__bd">
-
+      <section>
+        <h4>跨文档消息传递</h4>
+        <div>
+          iframe 嵌入页面之间的消息传递
+          postMessage 向内嵌框架中发送消息。
+        </div>
+      </section>
+      <!--&lt;!&ndash;音频&ndash;&gt;-->
+      <!--<audio src=""></audio>-->
+      <!--视频-->
+      <!--<video src="" ref="video"></video>-->
     </div>
   </div>
 </template>
@@ -12,8 +22,33 @@
     name: 'HelloWorld',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
       }
+    },
+    methods:{
+      click_back:function () {
+        this.$router.push({path:'/'});
+      },
+      //跨文档消息传递
+      postMessage:function () {
+
+      },
+      //vidio 属性
+      get_vedioPro:function () {
+        let vdo = this.$refs.video;
+        // video 的属性值
+
+      },
+      create_audio:function() {
+        let audio = new Audio();
+        console.log('audio',audio);
+        audio.addEventListener('canplaythrough',function(e) {
+          audio.play();
+        })
+      }
+    },
+    mounted() {
+      this.get_vedioPro();
+//      this.create_audio();
     }
   }
 </script>
